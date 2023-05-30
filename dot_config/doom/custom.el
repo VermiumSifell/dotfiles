28c0cb16d2192bd6581c28380dd3eae293c75de2
Vermium Sifell <vermium@zyner.org>
2023-05-30 19:02:19 +0200

    add awesome and doom

---
+++ dot_config/doom/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("77364a6e41e64138e2c9beb11a40ddfe73551587e86251afdc728edaf51931d9" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
