f2b0a2ae645113492052014be17bdf5b337da022
Vermium Sifell <vermium@zyner.org>
2023-11-15 01:43:42 +0100

    macchina and brightness keys

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+#echo # Print a empty row to make pfetch look better
+#pfetch
+macchina
+
 bind \e\[3\;5~ kill-word
 bind \cH backward-kill-word
 fish_ssh_agent
