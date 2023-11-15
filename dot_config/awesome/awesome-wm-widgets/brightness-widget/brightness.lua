cca4790ef0037e29c480db7f02396d9ddcc9e97a
Vermium Sifell <vermium@zyner.org>
2023-11-15 02:55:02 +0100

    improve brightness icon

--- dot_config/awesome/awesome-wm-widgets/brightness-widget/brightness.lua
+++ dot_config/awesome/awesome-wm-widgets/brightness-widget/brightness.lua
@@ -25,39 +25,41 @@ local dec_brightness_cmd
 local brightness_widget = {}
 
 local function show_warning(message)
-	naughty.notify({
-		preset = naughty.config.presets.critical,
-		title = "Brightness Widget",
-		text = message,
-	})
+    naughty.notify(
+        {
+            preset = naughty.config.presets.critical,
+            title = "Brightness Widget",
+            text = message
+        }
+    )
 end
 
 local function worker(user_args)
-	  local args = user_args or {}
+    local args = user_args or {}
 
-    local type = args.type or 'arc' -- arc or icon_and_text
-    local path_to_icon = args.path_to_icon or ICON_DIR .. 'brightness.svg'
+    local type = args.type or "arc" -- arc or icon_and_text
+    local path_to_icon = args.path_to_icon or ICON_DIR .. "brightness.svg"
     local font = args.font or beautiful.font
     local timeout = args.timeout or 100
 
-    local program = args.program or 'light'
+    local program = args.program or "light"
     local step = args.step or 5
     local base = args.base or 20
     local current_level = 0 -- current brightness value
     local tooltip = args.tooltip or false
     local percentage = args.percentage or false
     local rmb_set_max = args.rmb_set_max or false
-    if program == 'light' then
-        get_brightness_cmd = 'light -G'
-        set_brightness_cmd = 'light -S %d' -- <level>
-        inc_brightness_cmd = 'light -A ' .. step
-        dec_brightness_cmd = 'light -U ' .. step
-    elseif program == 'xbacklight' then
-        get_brightness_cmd = 'xbacklight -get'
-        set_brightness_cmd = 'xbacklight -set %d' -- <level>
-        inc_brightness_cmd = 'xbacklight -inc ' .. step
-        dec_brightness_cmd = 'xbacklight -dec ' .. step
-    elseif program == 'brightnessctl' then
+    if program == "light" then
+        get_brightness_cmd = "light -G"
+        set_brightness_cmd = "light -S %d" -- <level>
+        inc_brightness_cmd = "light -A " .. step
+        dec_brightness_cmd = "light -U " .. step
+    elseif program == "xbacklight" then
+        get_brightness_cmd = "xbacklight -get"
+        set_brightness_cmd = "xbacklight -set %d" -- <level>
+        inc_brightness_cmd = "xbacklight -inc " .. step
+        dec_brightness_cmd = "xbacklight -dec " .. step
+    elseif program == "brightnessctl" then
         get_brightness_cmd = "brightnessctl get"
         set_brightness_cmd = "brightnessctl set %d%%" -- <level>
         inc_brightness_cmd = "brightnessctl set +" .. step .. "%"
@@ -67,19 +69,21 @@ local function worker(user_args)
         return
     end
 
-    if type == 'icon_and_text' then
-        brightness_widget.widget = wibox.widget {
+    if type == "icon_and_text" then
+        brightness_widget.widget =
+            wibox.widget {
             {
                 {
-                    image = path_to_icon,
+                    text = "󰃟",
                     resize = false,
-                    widget = wibox.widget.imagebox,
+                    font = beautiful.icon_font,
+                    widget = wibox.widget.textbox
                 },
-                valign = 'center',
+                valign = "center",
                 layout = wibox.container.place
             },
             {
-                id = 'txt',
+                id = "txt",
                 font = font,
                 widget = wibox.widget.textbox
             },
@@ -88,20 +92,21 @@ local function worker(user_args)
             set_value = function(self, level)
                 local display_level = level
                 if percentage then
-                    display_level = display_level .. '%'
+                    display_level = display_level .. "%"
                 end
-                self:get_children_by_id('txt')[1]:set_text(display_level)
+                self:get_children_by_id("txt")[1]:set_text(display_level)
             end
         }
-    elseif type == 'arc' then
-        brightness_widget.widget = wibox.widget {
+    elseif type == "arc" then
+        brightness_widget.widget =
+            wibox.widget {
             {
                 {
                     image = path_to_icon,
                     resize = true,
-                    widget = wibox.widget.imagebox,
+                    widget = wibox.widget.imagebox
                 },
-                valign = 'center',
+                valign = "center",
                 layout = wibox.container.place
             },
             max_value = 100,
@@ -118,7 +123,6 @@ local function worker(user_args)
     else
         show_warning(type .. " type is not supported by the widget")
         return
-
     end
 
     local update_widget = function(widget, stdout, _, _, _)
@@ -129,11 +133,17 @@ local function worker(user_args)
 
     function brightness_widget:set(value)
         current_level = value
-        spawn.easy_async(string.format(set_brightness_cmd, value), function()
-            spawn.easy_async(get_brightness_cmd, function(out)
-                update_widget(brightness_widget.widget, out)
-            end)
-        end)
+        spawn.easy_async(
+            string.format(set_brightness_cmd, value),
+            function()
+                spawn.easy_async(
+                    get_brightness_cmd,
+                    function(out)
+                        update_widget(brightness_widget.widget, out)
+                    end
+                )
+            end
+        )
     end
     local old_level = 0
     function brightness_widget:toggle()
@@ -156,45 +166,84 @@ local function worker(user_args)
         end
     end
     function brightness_widget:inc()
-        spawn.easy_async(inc_brightness_cmd, function()
-            spawn.easy_async(get_brightness_cmd, function(out)
-                update_widget(brightness_widget.widget, out)
-            end)
-        end)
+        spawn.easy_async(
+            inc_brightness_cmd,
+            function()
+                spawn.easy_async(
+                    get_brightness_cmd,
+                    function(out)
+                        update_widget(brightness_widget.widget, out)
+                    end
+                )
+            end
+        )
     end
     function brightness_widget:dec()
-        spawn.easy_async(dec_brightness_cmd, function()
-            spawn.easy_async(get_brightness_cmd, function(out)
-                update_widget(brightness_widget.widget, out)
-            end)
-        end)
+        spawn.easy_async(
+            dec_brightness_cmd,
+            function()
+                spawn.easy_async(
+                    get_brightness_cmd,
+                    function(out)
+                        update_widget(brightness_widget.widget, out)
+                    end
+                )
+            end
+        )
     end
 
     brightness_widget.widget:buttons(
-            awful.util.table.join(
-                    awful.button({}, 1, function() brightness_widget:set(base) end),
-                    awful.button({}, 3, function() brightness_widget:toggle() end),
-                    awful.button({}, 4, function() brightness_widget:inc() end),
-                    awful.button({}, 5, function() brightness_widget:dec() end)
+        awful.util.table.join(
+            awful.button(
+                {},
+                1,
+                function()
+                    brightness_widget:set(base)
+                end
+            ),
+            awful.button(
+                {},
+                3,
+                function()
+                    brightness_widget:toggle()
+                end
+            ),
+            awful.button(
+                {},
+                4,
+                function()
+                    brightness_widget:inc()
+                end
+            ),
+            awful.button(
+                {},
+                5,
+                function()
+                    brightness_widget:dec()
+                end
             )
+        )
     )
 
     watch(get_brightness_cmd, timeout, update_widget, brightness_widget.widget)
 
     if tooltip then
         awful.tooltip {
-            objects        = { brightness_widget.widget },
+            objects = {brightness_widget.widget},
             timer_function = function()
                 return current_level .. " %"
-            end,
+            end
         }
     end
 
     return brightness_widget.widget
 end
 
-return setmetatable(brightness_widget, {
-	__call = function(_, ...)
-		return worker(...)
-	end,
-})
+return setmetatable(
+    brightness_widget,
+    {
+        __call = function(_, ...)
+            return worker(...)
+        end
+    }
+)
